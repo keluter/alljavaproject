427c7736a15bf6717179f21665e49ea64ef1afd5
keluter <104948848@qq.com>
2020-04-25 19:59:30 +0800

    can

---
+++ springdemo/src/test/java/com/hundsun/yy/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.hundsun.yy.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SpringdemoApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}

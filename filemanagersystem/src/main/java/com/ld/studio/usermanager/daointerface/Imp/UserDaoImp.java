881c6d7ab8cfb1d8a6c7d310d213b26fe16c1613
zhang <104948848@qq.com>
2020-04-25 21:42:44 +0800

    init

---
+++ filemanagersystem/src/main/java/com/ld/studio/usermanager/daointerface/Imp/UserDaoImp.java
@@ -0,0 +1,18 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package com.ld.studio.usermanager.daointerface.Imp;
+
+import com.ld.studio.usermanager.daointerface.UserDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author zhangwb15932
+ * @version userdaoimp: UserDaoImp, v0.1 2020年04月25日 21:27 zhang Exp $
+ */
+@Repository
+public class UserDaoImp implements UserDao {
+
+
+}
---
+++ filemanagersystem/src/main/java/com/ld/studio/usermanager/daointerface/UserDao.java
@@ -0,0 +1,14 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package com.ld.studio.usermanager.daointerface;
+
+/**
+ * @author zhangwb15932
+ * @version userdap: UserDao, v0.1 2020年04月25日 21:27 zhang Exp $
+ */
+public interface UserDao {
+
+
+}
